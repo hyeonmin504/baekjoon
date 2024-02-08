f07bed5f130b5bf0f900ac017b40cee20ae661a3
hyunmin504 <hyunmin504@naver.com>
2024-02-08 22:30:43 +0900

    number_sort_two

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-        growup();
+        number_sort_two();
     }
 
-    private static void growup() throws IOException {
+    private static void number_sort_two() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int count = Integer.parseInt(br.readLine());
-        int resultCount=0;
+        List<Integer> result2 = new ArrayList<>();
+
         for (int i = 0; i < count; i++) {
-            String words = br.readLine();
-            boolean result = true;
-            for (int j = 0; j < words.length()-1; j++) {
-                if (words.charAt(j)!=words.charAt(j+1)){
-                    if (words.substring(j+1).contains(String.valueOf(words.charAt(j)))) {
-                        result = false;
-                        break;
-                    }
-                }
-            }
-            if (result){
-                resultCount++;
-            }
+            result2.add(Integer.parseInt(br.readLine()));
         }
-        System.out.println(resultCount);
+        Collections.sort(result2);
+
+        for (int i = 0; i < count; i++) {
+            bw.write(result2.get(i)+"\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
+
+//    private static void growup() throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int count = Integer.parseInt(br.readLine());
+//        int resultCount=0;
+//        for (int i = 0; i < count; i++) {
+//            String words = br.readLine();
+//            boolean result = true;
+//            for (int j = 0; j < words.length()-1; j++) {
+//                if (words.charAt(j)!=words.charAt(j+1)){
+//                    if (words.substring(j+1).contains(String.valueOf(words.charAt(j)))) {
+//                        result = false;
+//                        break;
+//                    }
+//                }
+//            }
+//            if (result){
+//                resultCount++;
+//            }
+//        }
+//        System.out.println(resultCount);
+//    }
 }
